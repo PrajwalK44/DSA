e08a85f55e651ac5e8ee34571309a083b44b3bd4
PrajwalK44 <kulkarniprajwal543@gmail.com>
2024-09-11 10:12:31 +0530

    dsa

---
+++ leetcode/min_bit_flips.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int res=0;
+
+        int n=start ^ goal;
+
+        while(n){
+            res+=n & 1;
+            n >>=1;
+        }
+        return res;
+    }
+};
