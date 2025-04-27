2703d35594b505512ab1f266aca936c62b672a10
PrajwalK44 <153058777+PrajwalK44@users.noreply.github.com>
2025-04-28 02:17:13 +0530

    Update A_star.py

--- A_star.py
+++ A_star.py
@@ -1,48 +1,48 @@
+#astar
 import heapq
-
-
 def a_star(graph, start, goal, heuristics):
     visited = set()
-    open_list = [(heuristics[start], 0, start)]
+    open_list = [(heuristics[start], 0, start, [start])]
     
     while open_list:
-        f, g, node = heapq.heappop(open_list)
+        f, g, node, path = heapq.heappop(open_list)
         
         if node in visited:
             continue
-        print(f"Visiting {node} of heuristic: {heuristics[node]} with the cost {g} and total cost of {f}")
-        visited.add(node)
+        print(f"Visiting node {node} of heuristics: {heuristics[node]} of cost {g}")
         
         if node == goal:
-            print(f"Goal node {node} found with the total cost {f}")
+            print("\nGoal found")
+            print(f"Path: {' -> '.join(path)}")
+            print("Total cost ",cost)
             return
         
         for neighbour, cost in graph.get(node, []):
             if neighbour not in visited:
                 g_new = g+cost
-                f_new = g_new + heuristics[neighbour]
-                heapq.heappush(open_list, (f_new, g_new, neighbour))
-    
+                f_new = g_new+ heuristics[node]
+                heapq.heappush(open_list, (f_new, g_new, neighbour, path+[neighbour]))
+
 
 graph = {}
-n=int(input("Enter the number of nodes: "))
+n=int(input("Enter the number of nodes "))
 
 for _ in range(n):
-    node = input("enter the node: ")
+    node = input("Enter the node ")
     neighbours = []
-    num_neighbour = int(input(f"Enter the number of neighbours for {node}: "))
-    for _ in range(num_neighbour):
-        neighbour= input("Enter the neighbour node: ")
-        cost = int(input(f"Enter the cost to reach from {node} to {neighbour}"))
-        neighbours.append((neighbour, cost))
+    num_neighbours = int(input("Enter the number of neighbours "))
     
+    for _ in range(num_neighbours):
+        neighbour = input("Enter the neighbour ")
+        cost = int(input(f"Enter the cost from {node} to {neighbour}"))
+        neighbours.append((neighbour, cost))
     graph[node] = neighbours
 
 heuristics = {}
 for node in graph.keys():
-    heuristics[node] = int(input(f"ENter the heuristics valie of {node}: "))
-    
+    heuristics[node] = int(input(f"Enter the heuristics value for {node}"))
+
 start = input("Enter the starting node ")
-goal = input("Enterh the goal node ")
+goal = input("Enter the goal node ")
+
 a_star(graph, start, goal, heuristics)
-    
