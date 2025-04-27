003ba5a559c7091ddc19161653e7f0776e17b8b3
PrajwalK44 <153058777+PrajwalK44@users.noreply.github.com>
2025-04-28 01:20:41 +0530

    Update UCS.py

--- UCS.py
+++ UCS.py
@@ -1,41 +1,43 @@
+#UCS
 import heapq
-
 def ucs(graph, start, goal):
     visited = set()
-    pq=[(0, start)]
+    pq = [(0, start, [start])]
     
     while pq:
-        cost, node = heapq.heappop(pq)
+        cost, node, path = heapq.heappop(pq)
         
         if node in visited:
             continue
-        print(f"Visiting node {node}, Cost: {cost}")
-        visited.add(node)    
+        
+        print(f"Visiting node {node} Cost: {cost}")
+        visited.add(node)
         
         if node == goal:
-            print(f"Goal node {node} found with cost {cost}")
-            return
+            print("\nGoal node found!")
+            print(f"Path: {' -> '.join(path)}")
+            print(f"Total cost: {cost}")
+            return  # exit after finding the goal
         
         for neighbour, edge_cost in graph.get(node, []):
             if neighbour not in visited:
-                heapq.heappush(pq, (cost+edge_cost, neighbour))
+                heapq.heappush(pq, (cost+edge_cost, neighbour, path+[neighbour]))
 
-graph={}
-n=int(input("Enter the numebr of nodes in the graphh"))
+
+graph = {}
+
+n=int(input("Enter the number of nodes in the graph "))
 
 for _ in range(n):
-    node = input("Enter the node: ")
+    node = input("Enter the node ")
     neighbours = []
-    num_neighbours = int(input("Enter the number of neighbours: "))
-    
+    num_neighbours = int(input(f"Enter the number of neighbours of {node}"))
     for _ in range(num_neighbours):
-        neighbour = input("Enter the neihgbor")
+        neighbour = input(f"Enter the neighbour of {node} ")
         edge_cost = int(input(f"Enter the cost from {node} to {neighbour} "))
         neighbours.append((neighbour, edge_cost))
-    
     graph[node] = neighbours
 
-start = input("Enter the start node ")
-goal = input("Enter the goal node")
-
+start = input("Enter the starting node ")
+goal = input("Enter the goal node ")
 ucs(graph, start, goal)
