ad2499f258fba563c7544804eae09ef4e934e138
PrajwalK44 <153058777+PrajwalK44@users.noreply.github.com>
2025-04-27 17:26:52 +0530

    Update family_tree.pl

--- family_tree.pl
+++ family_tree.pl
@@ -5,12 +5,10 @@ parent(mary, anne).    % Mary is a parent of Anne
 parent(jim, ben).      % Jim is a parent of Ben
 
 % Rules
-% A person is a mother if they are a parent and their child is female.
 mother(X, Y) :-
     parent(X, Y),
     female(X).
 
-% A person is a father if they are a parent and their child is male.
 father(X, Y) :-
     parent(X, Y),
     male(X).
